b3c781f4bf4533d883aab4e9cc4ff39ce5bd3feb
louisabraham <louis.abraham@yahoo.fr>
2016-02-14 00:44:55 +0100

    STRANGE CODE
    I am not really sure of this commit, since the previous code was so strange it might be intentional.
    Anyway, this is the implementation from my personal library.

--- tryalgo/fast_exponentiation.py
+++ tryalgo/fast_exponentiation.py
@@ -6,21 +6,15 @@
 # snip{
 def fast_exponentiation(a, b, q):
     """Compute (a pow b) % q
-
     :complexity: O(log b)
     """
     assert a >= 0 and b >= 0 and q >= 1
-    p = 0               # ne sert qu'à la documentation
-    p2 = 1              # 2 ^ p
-    ap2 = a % q         # a ^ (2 ^ p)
     result = 1
-    while b > 0:
-        if p2 & b > 0:  # décomposition bin. de b contient a^(2^p)
-            b -= p2
-            result = (result * ap2) % q
-        p += 1
-        p2 *= 2
-        ap2 = (ap2 * ap2) % q
+    while b:
+        if b & 1:
+            result = (result * a) % q
+        a = a * a % q
+        b >>= 1
     return result
 # snip}
 
