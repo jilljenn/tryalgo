e8b0d67369545d7a9647cb34240010ef2fc61601
Jill-Jênn Vie <vie@jill-jenn.net>
2016-03-29 16:15:39 +0200

    Make find_cycle PEP8-compliant

--- tryalgo/dfs.py
+++ tryalgo/dfs.py
@@ -107,30 +107,29 @@ def dfs_grid(grid, i, j, mark='X', free='.'):
 
 
 def find_cycle(graph):
-  """find a cycle in an undirected graph
+    """find a cycle in an undirected graph
 
-  :param graph: undirected graph in listlist or listdict format
-  :returns: list of vertices in a cycle or None
-  :complexity: `O(|V|+|E|)`
-  """
-  n = len(graph)
-  prec  = [None] * n              # ancestor marks for visited vertices
-  for u in range(n):
-    if prec[u] is None:           # unvisited vertex
-      S = [u]                     # start new DFS
-      prec[u] = u                 # mark root (not necessary for this algorithm)
-      while S:
-        u = S.pop()
-        for v in graph[u]:        # for all neighbors
-          if v != prec[u]:        # except arcs to father in DFS tree
-            if prec[v] is not None:
-              cycle = [v, u]      # cycle found, (u,v) is a back edge
-              while u != prec[v] and u != prec[u]:  # for directed graphs
-                u = prec[u]       # climb up the tree
-                cycle.append(u)
-              return cycle
-            else:
-              prec[v] = u         # v is new vertex in tree
-              S.append(v)
-  return None
-
+    :param graph: undirected graph in listlist or listdict format
+    :returns: list of vertices in a cycle or None
+    :complexity: `O(|V|+|E|)`
+    """
+    n = len(graph)
+    prec = [None] * n  # ancestor marks for visited vertices
+    for u in range(n):
+        if prec[u] is None:  # unvisited vertex
+            S = [u]  # start new DFS
+            prec[u] = u  # mark root (not necessary for this algorithm)
+            while S:
+                u = S.pop()
+                for v in graph[u]:  # for all neighbors
+                    if v != prec[u]:  # except arcs to father in DFS tree
+                        if prec[v] is not None:
+                            cycle = [v, u]  # cycle found, (u,v) is a back edge
+                            while u != prec[v] and u != prec[u]:  # directed
+                                u = prec[u]  # climb up the tree
+                                cycle.append(u)
+                            return cycle
+                        else:
+                            prec[v] = u  # v is new vertex in tree
+                            S.append(v)
+    return None
