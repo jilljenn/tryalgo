229ce0af8dd4bcef306b5904c779e134449e0df2
louisabraham <louisabraham@users.noreply.github.com>
2017-09-01 08:43:18 +0200

    Add getkth method and visualization

--- tryalgo/skip_list.py
+++ tryalgo/skip_list.py
@@ -3,8 +3,10 @@ from random import random
 
 """
 Inspired by https://kunigami.blog/2012/09/25/skip-lists-in-python/
+count contains the gap between the positions (https://www.cs.bgu.ac.il/~ds112/wiki.files/ds112_ps7.pdf)
 """
 
+# TODO: add order_of_key
 
 class AbstractSkipList():
 
@@ -33,17 +35,36 @@ class AbstractSkipList():
         return self.find(key) is not None
 
     def _updateList(self, key):
-        update = [None] * len(self.next)
+        heigh = len(self.next)
+        update = [None] * heigh
+        count = [0] * heigh
+        c = -1
         x = self
-        for i in reversed(range(len(self.next))):
+        for i in reversed(range(heigh)):
             while x.next[i] is not None and x.next[i].key < key:
+                c += x.count[i]
                 x = x.next[i]
             update[i] = x
-        return update
+            count[i] = c
+        return update, count
+
+    def getkth(self, k):
+        "starts from 0"
+        if k >= len(self):
+            raise IndexError
+        k += 1  # self has index -1
+        h = len(self.next) - 1
+        x = self
+        while k:
+            while x.next[h] is None or x.count[h] > k:
+                h -= 1
+            k -= x.count[h]
+            x = x.next[h]
+        return x.key
 
     def nextNode(self, key, update=None):
         if update is None:
-            update = self._updateList(key)
+            update = self._updateList(key)[0]
         if update:
             ans = update[0].next[0]
             if ans is not None:
@@ -59,7 +80,7 @@ class AbstractSkipList():
 
     def lastKey(self, key):
         """lastKey(key) < key"""
-        update = self._updateList(key)
+        update = self._updateList(key)[0]
         return (update[0].key
                 if update
                 else None)
@@ -71,40 +92,56 @@ class AbstractSkipList():
                 else None)
 
     def insert(self, node):
+        # nup = len(self.next)
         while len(self.next) < len(node.next):
             self.next.append(None)
-        update = self._updateList(node.key)
+            self.count.append(self._len)
+        update, index = self._updateList(node.key)
+        # print(index)
+        # for i in range(nup, len(self.count)):
+        #     self.count[i] += 1
+        nindex = index[0] + 1
         if self.find(node.key, update) is None:
             self._len += 1
             for i in range(len(node.next)):
                 node.next[i] = update[i].next[i]
                 update[i].next[i] = node
+                node.count[i] = index[i] + update[i].count[i] + 1 - nindex
+                update[i].count[i] = nindex - index[i]
+            for i in range(len(node.next), len(update)):
+                update[i].count[i] += 1
 
     def remove(self, key):
-        update = self._updateList(key)
-        x = self.find(key, update)
-        if x is None:
+        update = self._updateList(key)[0]
+        node = self.find(key, update)
+        if node is None:
             raise KeyError
         self._len -= 1
-        for i in range(len(x.next)):
-            update[i].next[i] = x.next[i]
+        for i in range(len(node.next)):
+            update[i].next[i] = node.next[i]
+            update[i].count[i] += node.count[i] - 1
+        for i in range(len(node.next), len(update)):
+            update[i].count[i] -= 1
 
     @staticmethod
     def randomHeight():
-        r = 2 * random()
+        
+        p = 2
+        
+        r = p * random()
         ans = 1
-        while r > 1:
-            r = (r - 1) * 2
+        while r < 1:
+            r = r * p
             ans += 1
         return ans
 
 
 class SortedSet(AbstractSkipList):
 
-    Node = namedtuple('Node', 'key next')
+    Node = namedtuple('Node', 'key next count')
 
     def __init__(self, iterable=()):
-        self.head = self.Node(None, [])
+        self.head = self.Node(None, [], [])
         self._len = 0
         for i in iterable:
             self.add(i)
@@ -119,7 +156,8 @@ class SortedSet(AbstractSkipList):
             raise KeyError('pop from an empty set') from None
 
     def add(self, key):
-        super().insert(self.Node(key, [None] * self.randomHeight()))
+        height = self.randomHeight()
+        self.insert(self.Node(key, [None] * height, [0] * height))
 
     def __repr__(self):
         return '{%s}' % (', '.join(map(str, iter(self))))
@@ -127,10 +165,10 @@ class SortedSet(AbstractSkipList):
 
 class SortedDict(AbstractSkipList):
 
-    Node = namedtuple('Node', 'key val next')
+    Node = namedtuple('Node', 'key val next count')
 
     def __init__(self, iterable=()):
-        self.head = self.Node(None, None, [])
+        self.head = self.Node(None, None, [], [])
         self._len = 0
         if hasattr(iterable, 'keys') and hasattr(iterable, '__getitem__'):
             for i in iterable.keys():
@@ -154,10 +192,34 @@ class SortedDict(AbstractSkipList):
             self.remove(key)
         except KeyError:
             pass
-        super().insert(self.Node(key, value, [None] * self.randomHeight()))
+        height = self.randomHeight()
+        self.insert(self.Node(key, value, [None] * height, [0] * height))
 
     def __delitem__(self, key):
         self.remove(key)
 
     def __repr__(self):
         return '{%s}' % (', '.join('%s: %s' % (k, self[k]) for k in self))
+
+
+
+if __name__ == '__main__':
+    from random import sample
+
+    def display(a):
+        while a is not None:
+            print(a.key if a.key is not None else 'N', end=' | ')
+            print(*('(%s, %s)' % (i.key if i is not None else 'N', c)
+                    for i, c in zip(a.next, a.count)))
+            a = a.next[0]
+    n = SortedSet()
+    for i in sample(range(5), 5):
+        n.add(i)
+        display(n)
+        print(n.getkth(0))
+        print("----------")
+    for i in sample(range(5), 0):
+        n.remove(i)
+        display(n)
+        print("----------")
+    
