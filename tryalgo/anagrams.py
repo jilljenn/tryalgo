16558a46f9cd2735b2c9ce2061be230b0786fbf6
Christoph Dürr <xtof.durr@gmail.com>
2019-12-13 11:51:15 +0100

    improved readability of anagrams

--- tryalgo/anagrams.py
+++ tryalgo/anagrams.py
@@ -8,28 +8,24 @@ christoph dürr - jill-jênn vie - 2013-2019
 
 # snip{
 # pylint: disable=anomalous-backslash-in-string
-def anagrams(w):                   # w is a list of strings
+def anagrams(S):                   # S is a set of strings
     """group a list of words into anagrams
 
-    :param w: list of strings
-    :returns: list of lists
+    :param S: set or list of strings
+    :returns: list of lists of strings
 
     :complexity:
         :math:`O(n k log k)` in average, for n words of length at most k.
         :math:`O(n^2 k log k)` in worst case due to the usage of a dictionary.
     """
-    w = list(set(w))               # use set() to remove duplicates
-    d = {}
-    for i, word in enumerate(w):   # group words according to the signature
+    S = set(S)                     # remove duplicates
+    d = {}                         # maps s to list of words with signature s
+    for word in S:                 # group words according to the signature
         s = ''.join(sorted(word))  # calculate the signature
         if s in d:
-            d[s].append(i)         # append a word to an existing signature
+            d[s].append(word)      # append a word to an existing signature
         else:
-            d[s] = [i]             # add a new signature and its first word
-    # -- extract anagrams
-    answer = []
-    for s in d:
-        if len(d[s]) > 1:          # ignore words without anagram
-            answer.append([w[i] for i in d[s]])
-    return answer
+            d[s] = [word]          # add a new signature and its first word
+    # -- extract anagrams, ingoring anagram groups of size 1
+    return [d[s] for s in d if len(d[s]) > 1]
 # snip}
