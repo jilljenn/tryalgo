fa94ab5c8b65ba3a03d6ccba2abbca2834ac2923
xtof-durr <xtof-durr@users.noreply.github.com>
2025-12-23 21:18:48 +0000

    deploy: 46ddc449704c1328a16943e11e718c930dc1347f

---
+++ _downloads/f56cc920f0c5cf5f8683c22296826edd/arithm_expr_target.py
@@ -0,0 +1,13 @@
+"""
+Countdown
+=========
+"""
+
+from tryalgo.arithm_expr_target import arithm_expr_target
+
+arithm_expr_target([25, 50, 75, 100, 3, 6], 952)
+
+# %%
+# Returns :code:`((((75*3)*(100+6))-50)/25)=952`.
+# 
+# See on our blog the `original Countdown video <https://tryalgo.org/fr/2017/03/14/le-compte-est-bon/>`_ behind this example.
